8a4d430a227bc7255654347f62a23402f72db05a
vihaan035 <vihaangupta035@gmail.com>
2021-07-07 21:35:54 +0530

    First commit

--- src/com/company/triangle2.java
+++ src/com/company/triangle2.java
@@ -2,6 +2,7 @@
 //                                                                         two side    all sides    no sides
 package com.company;
 import java.util.Scanner;
+
 public class triangle2 {
     public static void main(String []args){
         Scanner my_sc = new Scanner(System.in);
@@ -13,14 +14,21 @@ public class triangle2 {
         System.out.printf("Enter the third value:");
         int z = my_sc.nextInt();
 
-        if(x==y&&y==z&&z==x){
-            System.out.println("This is an Equilateral triangle.");
-        }
-        else if(x==y||y==z||z==x){
-            System.out.println("This is an Isosceles triangle.");
+        if(x>y+z||y>z+x||z>x+y){
+            System.out.println("The values given cannot make a triangle.");
         }
         else{
-            System.out.println("This is a Scalene triangle.");
+            if(x==y&&y==z&&z==x){
+                System.out.println("This is an Equilateral triangle.");
+            }
+            else if(x==y||y==z||z==x){
+                System.out.println("This is an Isosceles triangle.");
+            }
+            else{
+                System.out.println("This is a Scalene triangle.");
+            }
         }
+
+
     }
 }
