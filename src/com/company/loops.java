5a12d71c74600acfdf3c572a51453b9dd659bd92
vihaan035 <vihaangupta035@gmail.com>
2021-07-11 21:03:10 +0530

    First commit

---
+++ src/com/company/loops.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class loops {
+    public static void main(String []args){
+        int i = 1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+//        int i;
+//        for (i=10;i>=1 ; i--){
+//            System.out.println(i);
+//        }
+    }
+}
