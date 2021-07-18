ba94e24354ef56fa537e476c83276e87e427ad25
vihaan035 <vihaangupta035@gmail.com>
2021-07-18 17:22:05 +0530

    extracting_digits

--- src/com/company/extracting_digits.java
+++ src/com/company/extracting_digits.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class extracting_digits {
     public static void main(String []args){
         Scanner my_sc = new Scanner(System.in);
-        System.out.println("Enter a number to find how long it is.");
+        System.out.println("Enter a number to find the addition of the numbers.");
         int input = my_sc.nextInt();
         int i = input;
-        int x = 0;
+        int sum = 0;
         int digits = 0;
         while (i>0){
-            System.out.println(i);
             digits++;
+            sum = sum+i%10;
             i = i/10;
         }
-        System.out.println(x);
+        System.out.println(sum);
 //        System.out.println(digits);
     }
 }
