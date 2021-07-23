4a2004249fbce1333f5b04ed47af96ec6635dd83
vihaan035 <vihaangupta035@gmail.com>
2021-07-23 17:49:00 +0530

    factorial.java123

--- src/com/company/match_stick.java
+++ src/com/company/match_stick.java
@@ -1,4 +1,18 @@
+//the user will enter 5 numbers and we have to add them using while loop.
 package com.company;
+import java.util.Scanner;
 
 public class match_stick {
+    public static void main(String []args){
+        Scanner my_sc = new Scanner(System.in);
+        int x = 0;
+        int choice = 5;
+        System.out.println("Enter Number of matchboxes and this program will print total number of matchsticks.");
+        while (choice>0){
+            int input = my_sc.nextInt();
+            choice--;
+            x = x + input;
+        }
+        System.out.println("The Total Number of Mathsticks: " + x);
+    }
 }
---
+++ src/com/company/smallest_number.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class smallest_number {
+
+}
