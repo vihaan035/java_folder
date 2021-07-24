ce9f0ecfc89ef2b5968680d63708b1388e4347b0
vihaan035 <vihaangupta035@gmail.com>
2021-07-24 11:08:43 +0530

    factorial.java123

--- src/com/company/smallest_number.java
+++ src/com/company/smallest_number.java
@@ -1,5 +1,26 @@
 package com.company;
-
+import java.util.Scanner;
 public class smallest_number {
-
+    public static void main(String []args){
+        Scanner my_sc = new Scanner(System.in);
+        int smallest = 0;
+        int largest = 0;
+        int choice = 4;
+        System.out.println("Enter 5 numbers and this program will tell you the smallest and the largest number.");
+        int input1 = my_sc.nextInt();
+        smallest=input1;
+        largest=input1;
+        while(choice>0){
+            int input = my_sc.nextInt();
+            choice--;
+            if(input<smallest){
+                smallest=input;
+            }
+            if(input>largest){
+                largest=input;
+            }
+        }
+        System.out.println("The smallest Number is: " + smallest);
+        System.out.println("The Largest Number is: " + largest);
+    }
 }
