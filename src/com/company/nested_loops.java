99264c722679d2ec1ec3932fcc1c4e153a6886c5
vihaan035 <vihaangupta035@gmail.com>
2021-07-28 18:03:54 +0530

    factorial.java123

--- src/com/company/nested_loops.java
+++ src/com/company/nested_loops.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class nested_loops {
+    public static void main(String []args){
+        for(int i = 1; i <=10; i++){
+            for(int x = 1; x<=10; x++){
+                System.out.print(i*x + "\t");
+            }
+            System.out.println();
+//            changes line^^^^
+        }
+    }
 }
---
+++ src/com/company/tables.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class tables {
+    public static void main(String []args){
+        int var = 1;
+        int mul = 1;
+        int until = 10;
+        while (until > 0){
+
+            until--;
+            mul++;
+            var++;
+        }
+    }
+}
