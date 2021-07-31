9bef38571c88b21bbee7a2d41fb025678910ce53
vihaan035 <vihaangupta035@gmail.com>
2021-07-31 17:00:09 +0530

    factorial.java123

--- src/com/company/pattern3.java
+++ src/com/company/pattern3.java
@@ -3,7 +3,7 @@ package com.company;
 public class pattern3 {
     public static void main(String []args){
         int a = 1;
-        for(int x =4; x>0;x--){
+        for(int x =1; x<=4;x++){
             for(int i = 1 ;i<=4;i++){
                 System.out.print(a+" ");
                 a++;
--- src/com/company/pattern4.java
+++ src/com/company/pattern4.java
@@ -1,5 +1,14 @@
 package com.company;
 
 public class pattern4 {
-
+    public static void main(String []args){
+        int a = 1;
+        for(int x =1; x<=4;x++){
+            for(int i = 1 ;i<=x;i++){
+                System.out.print("*");
+                a++;
+            }
+            System.out.println();
+        }
+    }
 }
